14ddfc764750efc41447d243e0f991cf13f5368e
olezdev <olezdev@gmail.com>
2023-12-19 00:08:43 -0300

    Edit model for add count and log

--- models/User.js
+++ models/User.js
@@ -5,7 +5,27 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
-  }
+  },
+  count: {
+    type: Number,
+    default: 0
+  },
+  log: [
+    {
+      description: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      }
+    }
+  ]
 })
 
 const User = mongoose.model('User', userSchema);
