3a2e1e54428a95bf6fc184cef6d098878e52b2d9
olezdev <olezdev@gmail.com>
2023-12-18 22:07:43 -0300

    add connection mongoose and userRoutes

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const userRoutes = require('./routes/userRoutes')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
+const app = express()
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use('/api/users', userRoutes)
 
 
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+const PORT = process.env.PORT ?? 3000
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port  ${PORT}`)
 })
